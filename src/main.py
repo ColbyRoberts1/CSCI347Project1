e0b78eb250b831c6d84d3955c6f00c3e62cbd311
Hank Breckenridge <breckenridgeh2@gmail.com>
2023-03-02 13:50:33 -0700

    Improve comments

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 # Loads the data into a Panda's dataframe, sets column names,
 def getData():
+    # Load and format data as a dataframe
     df = pd.read_csv(
         "./auto-mpg.csv", 
         names=["MPG", "Cylinders", "Displacement", "Horsepower", "Weight", "Acceleration", "Model Year", "Origin", "Car Make/Model"], 
-        doublequote=True,
     )
+    # Correct the datatype of Displacement
     df["Displacement"] = df["Displacement"].astype(float)
     return df
 
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,19 @@
 import numpy as np
 
+# Computes the sample covariance of two numpy arrays
 def sample_covariance(arr1: np.ndarray, arr2: np.ndarray):
     return np.cov(arr1, arr2)[0][1]
 
+# Computes the correlation coefficient of two numpy arrays
 def correlation(arr1: np.ndarray, arr2: np.ndarray):
     return np.corrcoef(arr1, arr2)[0][1]
 
 if __name__ == "__main__":
+    # Test inputs
     THRESHOLD = 0.01
     arr1 = np.array([1, 2, 3, 4, 5, 6])
     arr2 = np.array([10, 20, 27, 20, 18, 6])
+
     # Test sample covariance
     covar = sample_covariance(arr1, arr2)
     if (covar + 3.3 < THRESHOLD):
@@ -17,6 +21,7 @@ if __name__ == "__main__":
     else:
         print("Sample covariance test failed!")
         print(f"    covariance calculated: {covar}")
+
     # Test correlation coefficient
     corr = correlation(arr1, arr2)
     if (corr + 0.23 < THRESHOLD):
