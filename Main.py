12862eac4929cbb20319b0530092ab48a754801d
Colby Roberts <robertsoc@hotmail.com>
2023-03-02 01:34:42 -0700

    finished array compilation code

--- Main.py
+++ Main.py
@@ -4,27 +4,29 @@ import numpy as np
 #THIS IS JUST TO COMPILE THE DATA FILE INTO A NUMPY ARRAY OF OBJECTS. IF YOU FIND AN EASIER WAY TO DO THIS LET ME KNOW OR JUST CHANGE IT LMAO
 def makeArr(file):
     fileLines = file.readlines()
-    arr = np.empty((len(fileLines),9), dtype=object)
+    arr = np.empty((len(fileLines)-1,9))
     lineCounter = 0
+    carModel = 0
     while lineCounter < len(fileLines)-1 :
         lineSplit = fileLines[lineCounter].split()
         wordCounter = 0
         while wordCounter < 9:
+            #THIS IS FOR THE PURPOSE OF LABEL-ENCODING THE CAR MODEL NAME. BECAUSE THEY ARE ALL DIFFERENT IT WILL JUST BE ITERATING BY ONE EACH TIME
             if wordCounter == 8:
-                carCompName = ''
-                while wordCounter < len(lineSplit):
-                    carCompName = carCompName + " " + lineSplit[wordCounter]
-                    wordCounter += 1
-                carCompName = carCompName.replace('"', '')
-                carCompName = carCompName[1:]
-                arr[lineCounter, 8] = carCompName
+                arr[lineCounter, wordCounter] = carModel
+                carModel += 1
+                wordCounter = 9
             else:
-               arr[lineCounter, wordCounter] = lineSplit[wordCounter]
-               wordCounter += 1
+                if lineSplit[wordCounter] == '?':
+                    arr[lineCounter, wordCounter] = 0
+                else: 
+                    arr[lineCounter, wordCounter] = lineSplit[wordCounter]
+                wordCounter += 1
         lineCounter += 1
-    return arr
-                
+    return arr          
         
+def findMean(arr):
+    
 with open('auto-mpg.data', 'r') as file:
     data = makeArr(file)
     print(data)
